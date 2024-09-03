8af998ca7945b298cff0cd2e82ed4edfa6c6d7e2
Yaroslav Khutornoy <morse1995@mail.ru>
2024-09-03 21:08:44 +0300

    Remove old srcs

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from simulation.scene.scene import Scene
-from simulation.scene.reader import SceneReader
-
-
-scene = Scene()
-scene_reader = SceneReader(scene)
-scene_reader.read("config/scene/scene_1.json")
